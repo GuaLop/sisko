65fa66fed3ca28d8879cd1123d524e5a6e8372bb
Guadalupe Lopez <lvg051091@gmail.com>
2022-09-26 10:53:45 -0500

    Feat:uso de script externo

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko papi saoko")
+document.getElementById("generation").style.color="grey";
+function colorTitulo(){
+  console.log("pulsar botón de Generation");
+  document.getElementById("generation").style.color="blue";
+}
